35ff24c942bb57070684506552cf120def44b3c1
RT1811 <ritwick.thebest@gmail.com>
2026-08-30 10:03:33 -0700

    Fix passport user schema

--- config/passport.js
+++ config/passport.js
@@ -31,7 +31,7 @@ function initialize(passport) {
 
     passport.deserializeUser(async (id, done) => {
         try {
-            const result = await pool.query("SELECT * FROM public.users WHERE id = $1", [id]);
+            const result = await pool.query("SELECT * FROM public.  users WHERE id = $1", [id]);
             done(null, result.rows[0]);
         } catch(err) {
             done(err);
